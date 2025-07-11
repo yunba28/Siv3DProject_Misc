75532f5cb8c726b4c74ab496bf62928bf4c2cd58
yunba28 <70497005+yunba28@users.noreply.github.com>
2025-07-11 20:18:41 +0900

    初コミット。XMLWritterの追加

---
+++ Siv3DProject_Misc/Src/XML/XML.cpp
@@ -0,0 +1,31 @@
+﻿#include "XML.hpp"
+
+void xml::TestCode()
+{
+	XMLWriter xml;
+	auto root = xml.CreateNode(U"root");
+	{
+		auto characters = xml.CreateNode(U"characters", *root);
+		auto mob1 = xml.CreateNode(U"character", *characters);
+		mob1->AddAttribute(U"name", U"mob1")
+			.AddAttribute(U"age", 20)
+			.AddAttribute(U"sex", U"male");
+		auto mob2 = xml.CreateNode(U"character", *characters);
+		mob2->AddAttribute(U"name", U"mob2")
+			.AddAttribute(U"age", 20)
+			.AddAttribute(U"sex", U"female");
+	}
+	{
+		auto skils = xml.CreateNode(U"skils", *root);
+		auto fire = xml.CreateNode(U"skil", *skils);
+		fire->AddAttribute(U"name", "fire")
+			.AddAttribute(U"cost", 8)
+			.AddAttribute(U"power", 15);
+		auto ice = xml.CreateNode(U"skil", *skils);
+		ice->AddAttribute(U"name", "ice")
+			.AddAttribute(U"cost", 10)
+			.AddAttribute(U"power", 20);
+	}
+
+	xml.Save(U"xml/test.xml");
+}
---
+++ Siv3DProject_Misc/Src/XML/XML.hpp
@@ -0,0 +1,199 @@
+﻿#pragma once
+
+#include <Siv3D.hpp>
+
+namespace xml
+{
+	class XMLWriter;
+
+	class XMLNode
+	{
+	public:
+
+		XMLNode(String NewTag, XMLNode* ParentNode = nullptr)
+			: Tag(NewTag)
+		{
+			SetupParent(ParentNode);
+		}
+
+		XMLNode& AddAttribute(const String& Name, const char32* Value)
+		{
+			Attributes.emplace_back(Name, Value);
+			return *this;
+		}
+
+		XMLNode& AddAttribute(const String& Name, const String& Value)
+		{
+			Attributes.emplace_back(Name, Value);
+			return *this;
+		}
+
+		XMLNode& AddAttribute(const String& Name, const StringView Value)
+		{
+			Attributes.emplace_back(Name, Value);
+			return *this;
+		}
+
+		XMLNode& AddAttribute(const String& Name, int32 Value)
+		{
+			Attributes.emplace_back(Name, ToString(Value));
+			return *this;
+		}
+
+		XMLNode& AddAttribute(const String& Name, double Value)
+		{
+			Attributes.emplace_back(Name, ToString(Value));
+			return *this;
+		}
+
+		XMLNode& AddAttribute(const String& Name, bool Value)
+		{
+			Attributes.emplace_back(Name, ToString(Value));
+			return *this;
+		}
+
+	private:
+
+		void SetupParent(XMLNode* ParentNode)
+		{
+			if (ParentNode)
+			{
+				Parent = ParentNode;
+				Parent->Children.push_back(this);
+			}
+		}
+
+	private:
+
+		friend class XMLWriter;
+
+		String Tag;
+
+		Array<std::pair<String, String>> Attributes;
+
+		XMLNode* Parent;
+
+		Array<XMLNode*> Children;
+
+	};
+
+	class XMLWriter
+	{
+	public:
+
+		class NodeProxy
+		{
+		public:
+
+			NodeProxy(XMLNode& NodeRef)
+				: Node(NodeRef)
+			{
+			}
+
+			XMLNode* operator->()const
+			{
+				return &Node;
+			}
+
+			XMLNode* operator*()const
+			{
+				return &Node;
+			}
+
+		private:
+
+			XMLNode& Node;
+
+		};
+
+		XMLWriter() = default;
+		~XMLWriter()
+		{
+
+		}
+
+	public:
+
+		NodeProxy CreateNode(const String& Tag, XMLNode* ParentNode = nullptr)
+		{
+			XMLNode* newNode = new XMLNode(Tag, ParentNode);
+			AllNodes.push_back(newNode);
+			return NodeProxy(*newNode);
+		}
+
+		void Save(FilePath SavePath)
+		{
+			String xml;
+			xml += U"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+			Array<XMLNode*> rootNodes = AllNodes.filter(
+				[](XMLNode* Item)->bool
+				{
+						return Item && Item->Parent == nullptr;
+				}
+			);
+
+			for (auto& node : rootNodes)
+			{
+				ConvertToXML(xml, node, 0);
+			}
+
+			TextWriter writer(SavePath);
+			writer.write(xml);
+			writer.close();
+		}
+
+		void Clear()
+		{
+			for (auto& node : AllNodes)
+			{
+				if (!node)
+					continue;
+				delete node;
+				node = nullptr;
+			}
+			AllNodes.release();
+		}
+
+	private:
+
+		void ConvertToXML(String& OutXML, XMLNode* Node, int32 Level)
+		{
+			if (!Node)
+				return;
+
+			String indent;
+			for (int32 i = 0; i < Level; ++i)
+			{
+				indent += U"\t";
+			}
+
+			OutXML += U"{}<{}"_fmt(indent, Node->Tag);
+			for (auto& [tag, value] : Node->Attributes)
+			{
+				OutXML += U" {}=\"{}\""_fmt(tag, value);
+			}
+
+			if (Node->Children.empty())
+			{
+				OutXML += U"/>\n";
+			}
+			else
+			{
+				OutXML += U">\n";
+				for (auto& child : Node->Children)
+				{
+					ConvertToXML(OutXML, child, Level + 1);
+				}
+				OutXML += U"{}</{}>\n"_fmt(indent, Node->Tag);
+			}
+		}
+
+	private:
+
+		Array<XMLNode*> AllNodes;
+
+	};
+
+	void TestCode();
+}
